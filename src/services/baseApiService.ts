eafd147df52a2f8fce9155a83eba1fec61f4ba46
Sergiu Zagoret <szagoret@complexica.com>
2021-12-26 23:53:21 +0200

    change base url

--- src/services/baseApiService.ts
+++ src/services/baseApiService.ts
@@ -2,6 +2,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 const BASE_URL = process.env.API_BASE_URL || '';
 
+console.log(BASE_URL);
 export const baseApiService = createApi({
     baseQuery: fetchBaseQuery({baseUrl: `${BASE_URL}/api`}),
     tagTypes: ['SongFiles'],
